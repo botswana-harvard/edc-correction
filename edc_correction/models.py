14374d9b387b5399ece53b967ad3007bf449e2ef
Gofaone Magodi <magodign@magodign.local>
2017-06-15 08:04:14 +0200

    edc-correction

--- edc_correction/models.py
+++ edc_correction/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from edc_base.model_mixins import BaseUuidModel
+from django.core.validators import RegexValidator
 
 
 class HouseholdMember(BaseUuidModel):
@@ -16,6 +17,22 @@ class HouseholdMember(BaseUuidModel):
         null=True,
         blank=True)
 
+    initials = models.CharField(
+        validators=[RegexValidator(
+            regex=r'^[A-Z]{2,3}$',
+            message='Ensure initials consist of letters only in upper case, no spaces.'), ],
+        null=True,
+        max_length=3,
+        blank=True)
+
+    new_initials = models.CharField(
+        validators=[RegexValidator(
+            regex=r'^[A-Z]{2,3}$',
+            message='Ensure initials consist of letters only in upper case, no spaces.'), ],
+        null=True,
+        max_length=3,
+        blank=True)
+
 
 class EnrollmentChecklist(BaseUuidModel):
 
@@ -25,6 +42,9 @@ class EnrollmentChecklist(BaseUuidModel):
 class SubjectConsent(BaseUuidModel):
 
     last_name = models.CharField(
+        max_length=25)
+
+    new_last_name = models.CharField(
         max_length=25)
 
     household_member = models.ForeignKey(HouseholdMember)
--- edc_correction/python_scripts.py
+++ edc_correction/python_scripts.py
@@ -1,24 +1,39 @@
 from .models import HouseholdMember
+from edc_correction.models import SubjectConsent
 
 
 class UpdatePII:
 
-    def update_first_name(self, subject_identifier=None, new_first_name=None):
+    def __init__(self, subject_identifier=None, **fields):
+
         self.subject_identifier = subject_identifier
+
+    def update_fields(self):
+        pass
+
+    def update_first_name(self, new_first_name=None):
         self.new_first_name = new_first_name
         for member in HouseholdMember.objects.filter(subject_identifier=self.subject_identifier):
             member.first_name = self.new_first_name
             member.save()
         return member.first_name
 
-    def update_last_name(self):
-        pass
+    def update_last_name(self, new_last_name=None):
+        self.new_last_name = new_last_name
+        for member in SubjectConsent.objects.filter(subject_identifier=self.subject_identifier):
+            member.last_name = self.new_last_name
+            member.save()
+        return member.last_name
 
     def update_age_in_years(self):
         pass
 
-    def update_initials(self):
-        pass
+    def update_initials(self, first_name, last_name):
+        for member in HouseholdMember.objects.filter(subject_identifier=self.subject_identifier):
+            new_initials = f'{first_name[0]}{last_name[0]}'
+            member.initials = new_initials
+            member.save()
+        return member.initials
 
     def update_gender(self):
         pass
@@ -35,5 +50,16 @@ class UpdatePII:
     def update_guardian_name(self):
         pass
 
-    def update_user_modified(self):
+    def update_enrollment_and_consent(self):
         pass
+
+    def update_user_modified(self):
+        user_modified = None
+        if not self.id:
+            user_modified = self.user_created
+        else:
+            user_modified = self.user_modified
+        return user_modified
+
+    class Meta:
+        abstract = True
