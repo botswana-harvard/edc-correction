c94daa628afb37bbf3f5d453193478a830485cfa
Coulson Thabo Kgathi <ckgathi@gmail.com>
2017-09-02 14:59:22 +0200

    update database settings

--- edc_correction/settings.py
+++ edc_correction/settings.py
@@ -86,11 +86,9 @@ WSGI_APPLICATION = 'edc_correction.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': os.path.join(BASE_DIR, 'mysql.conf'),
-        },
-    },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
--- edc_correction/tests/test_views.py
+++ edc_correction/tests/test_views.py
@@ -1,9 +1,7 @@
-# from django.apps import apps as django_apps
 from django.test import TestCase
 from django.test.client import RequestFactory
-# from django.views.generic.base import ContextMixin
-# from django.test.utils import override_settings
-from edc_correction.views import HomeView
+
+from ..views import HomeView
 
 
 class TestHomeView(TestCase):
